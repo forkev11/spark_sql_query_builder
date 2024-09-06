ae4f4e96c68cd01e94ad599b8f068aec5cd82c35
Ivan Warlock <Ivan.Warlock@ibm.com>
2024-09-06 13:42:16 -0600

    first draw

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,9 +1,16 @@
 package com.spark.main
 
+import  constants.constants
+import controllers.queryBuilderCntrol
+
 object main {
     def main(args: Array[String]): Unit = {
         try {
-            println("Hola mundo");
+            val query_builder_cntrol = new queryBuilderCntrol();
+            val mapQueries : Map[String, List[Any]] = constants.queryMap("query_sample_1");
+            val selectString : String = query_builder_cntrol.create_Select_Section(mapQueries("select"));
+            println(selectString)
+            
         } catch {
             case ex: Exception => {println("Exception Occurred [main]: "+ex.printStackTrace());}
         }// catch closure
